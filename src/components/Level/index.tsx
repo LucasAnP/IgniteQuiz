7622a29f3ffc78d5952a012b520eb3cd1aa7d5a9
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-09-14 18:16:20 -0300

    Creation of pressable animated

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,4 +1,5 @@
-import { TouchableOpacityProps, Text, Pressable } from "react-native";
+import { useEffect } from "react";
+import { TouchableOpacityProps, Pressable } from "react-native";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
@@ -6,9 +7,10 @@ import Animated, {
   interpolateColor,
 } from "react-native-reanimated";
 
+const PressableAnimated = Animated.createAnimatedComponent(Pressable);
+
 import { THEME } from "../../styles/theme";
 import { styles } from "./styles";
-import { useEffect } from "react";
 
 const TYPE_COLORS = {
   EASY: THEME.COLORS.BRAND_LIGHT,
@@ -67,20 +69,21 @@ export function Level({
   }, [isChecked]);
 
   return (
-    <Pressable onPressIn={onPressIn} onPressOut={onPressOut} {...rest}>
-      <Animated.View
-        style={[
-          styles.container,
-          animatedContainerStyle,
-          {
-            borderColor: COLOR,
-          },
-        ]}
-      >
-        <Animated.Text style={[styles.title, animatedTextStyle]}>
-          {title}
-        </Animated.Text>
-      </Animated.View>
-    </Pressable>
+    <PressableAnimated
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
+      style={[
+        styles.container,
+        animatedContainerStyle,
+        {
+          borderColor: COLOR,
+        },
+      ]}
+      {...rest}
+    >
+      <Animated.Text style={[styles.title, animatedTextStyle]}>
+        {title}
+      </Animated.Text>
+    </PressableAnimated>
   );
 }
