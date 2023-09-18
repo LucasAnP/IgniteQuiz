168eae82f8cbb5b60f2951575625e5d389bbec74
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-09-18 20:32:33 -0300

    Micro-interactions: exect js with animation

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -16,7 +16,9 @@ import { OutlineButton } from "../../components/OutlineButton";
 import Animated, {
   Easing,
   Extrapolate,
+  event,
   interpolate,
+  runOnJS,
   useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
@@ -34,6 +36,7 @@ interface Params {
 type QuizProps = (typeof QUIZ)[0];
 
 const CARD_INCLINATION = 10;
+const CARD_SKIP_AREA = -200;
 
 export function Quiz() {
   const [points, setPoints] = useState(0);
@@ -176,7 +179,10 @@ export function Quiz() {
         cardPosition.value = event.translationX;
       }
     })
-    .onEnd(() => {
+    .onEnd((event) => {
+      if (event.translationX < CARD_SKIP_AREA) {
+        runOnJS(handleSkipConfirm)();
+      }
       cardPosition.value = withTiming(0);
     });
 
